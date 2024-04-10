54129a8bf10eb95db44d0828c43893977762dd63
Robert St. Louis <stlouis9@gmail.com>
2024-04-09 19:55:03 -0700

    added dir

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -1,52 +1,61 @@
-def call(dockerRepoName, imageName, portNum) {
+def call(dockerRepoName, imageName, portNum, service) {
     pipeline {
-    agent any
-    parameters {
-        booleanParam(defaultValue: false, description: 'Deploy the App', name: 'DEPLOY')
-    }
-    stages {
-        stage('Build') {
-            steps {
-                
-                sh 'if [ -d ".venv" ]; then rm -Rf .venv; fi'
-                sh 'python3 -m venv .venv'
-                sh '. ./.venv/bin/activate'
-                sh 'pip install -r requirements.txt --break-system-packages'
-                sh 'pip install --upgrade flask --break-system-packages'
-            }
+        agent any
+        parameters {
+            booleanParam(defaultValue: false, description: 'Deploy the App', name: 'DEPLOY')
         }
-        stage('Python Lint') {
-            steps {
-                sh 'pylint --fail-under 5 *.py'
+        stages {
+            stage('Build') {
+                steps {
+                    dir(service) {
+                        sh 'if [ -d ".venv" ]; then rm -Rf .venv; fi'
+                        sh 'python3 -m venv .venv'
+                        sh '. ./.venv/bin/activate'
+                        sh 'pip install -r requirements.txt --break-system-packages'
+                        sh 'pip install --upgrade flask --break-system-packages'
+                    }
+                }
             }
-        }
-        stage('Security Check'){
-            steps {
-              //add snyk or safety?
-              sh 'echo hello'
+            stage('Python Lint') {
+                steps {
+                    dir(service){
+                        sh 'pylint --fail-under 5 *.py'
+                    }
+                }
             }
-        }
-        stage('Package') {
-            when {
-                expression { env.GIT_BRANCH == 'origin/main' }
+            stage('Security Check') {
+                steps {
+                    dir(service){
+                        //add snyk or safety?
+                        sh 'echo hello'
+                    }
+                }
             }
-            steps {
-                withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
-                    sh "docker login -u 'stlouis9' -p '$TOKEN' docker.io"
-                    sh "docker build -t ${dockerRepoName}:latest --tag stlouis9/${dockerRepoName}:${imageName} ."
-                    sh "docker push stlouis9/${dockerRepoName}:${imageName}"
+            stage('Package') {
+                when {
+                    expression { env.GIT_BRANCH == 'origin/main' }
+                }
+                steps {
+                    dir(service) {
+                        withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
+                            sh "docker login -u 'stlouis9' -p '$TOKEN' docker.io"
+                            sh "docker build -t ${dockerRepoName}:latest --tag stlouis9/${dockerRepoName}:${imageName} ."
+                            sh "docker push stlouis9/${dockerRepoName}:${imageName}"
+                        }
+                    }
+                }
+            }
+            stage('Deliver') {
+                when {
+                    expression { params.DEPLOY }
+                }
+                steps {
+                    dir(service){
+                        sh "docker stop ${dockerRepoName} || true && docker rm ${dockerRepoName} || true"
+                        sh "docker run -d -p ${portNum}:${portNum} --name ${dockerRepoName} ${dockerRepoName}:latest"
+                    }
                 }
             }
         }
-        stage('Deliver') {
-            when {
-                expression { params.DEPLOY }
-            }
-            steps {
-                sh "docker stop ${dockerRepoName} || true && docker rm ${dockerRepoName} || true"
-                sh "docker run -d -p ${portNum}:${portNum} --name ${dockerRepoName} ${dockerRepoName}:latest"
-            }
-        }
     }
-}
 }
